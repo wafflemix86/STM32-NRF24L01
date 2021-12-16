11ca81756156318591287b7f9d19396795043367
evan <evan.ohlsen@gmail.com>
2021-12-15 22:52:13 -0600

    initial commit

---
+++ Inc/nRF24L01.h
@@ -0,0 +1,164 @@
+#ifndef nRF24L01_H_
+#define nRF24L01_H_
+
+#include "stm32l0xx_hal.h"
+#include "stdint.h"
+
+/* Memory Map */
+#define NRF_CONFIG  0x00
+#define EN_AA       0x01
+#define EN_RXADDR   0x02
+#define SETUP_AW    0x03
+#define SETUP_RETR  0x04
+#define RF_CH       0x05
+#define RF_SETUP    0x06
+#define NRF_STATUS  0x07
+#define OBSERVE_TX  0x08
+#define CD          0x09
+#define RX_ADDR_P0  0x0A
+#define RX_ADDR_P1  0x0B
+#define RX_ADDR_P2  0x0C
+#define RX_ADDR_P3  0x0D
+#define RX_ADDR_P4  0x0E
+#define RX_ADDR_P5  0x0F
+#define TX_ADDR     0x10
+#define RX_PW_P0    0x11
+#define RX_PW_P1    0x12
+#define RX_PW_P2    0x13
+#define RX_PW_P3    0x14
+#define RX_PW_P4    0x15
+#define RX_PW_P5    0x16
+#define FIFO_STATUS 0x17
+#define DYNPD       0x1C
+#define FEATURE     0x1D
+
+/* Bit Mnemonics */
+#define MASK_RX_DR  6
+#define MASK_TX_DS  5
+#define MASK_MAX_RT 4
+#define EN_CRC      3
+#define CRCO        2
+#define PWR_UP      1
+#define PRIM_RX     0
+#define ENAA_P5     5
+#define ENAA_P4     4
+#define ENAA_P3     3
+#define ENAA_P2     2
+#define ENAA_P1     1
+#define ENAA_P0     0
+#define ERX_P5      5
+#define ERX_P4      4
+#define ERX_P3      3
+#define ERX_P2      2
+#define ERX_P1      1
+#define ERX_P0      0
+#define AW          0
+#define ARD         4
+#define ARC         0
+#define PLL_LOCK    4
+#define CONT_WAVE   7
+#define RF_DR       3
+#define RF_PWR      6
+#define RX_DR       6
+#define TX_DS       5
+#define MAX_RT      4
+#define RX_P_NO     1
+#define TX_FULL     0
+#define PLOS_CNT    4
+#define ARC_CNT     0
+#define TX_REUSE    6
+#define FIFO_FULL   5
+#define TX_EMPTY    4
+#define RX_FULL     1
+#define RX_EMPTY    0
+#define DPL_P5      5
+#define DPL_P4      4
+#define DPL_P3      3
+#define DPL_P2      2
+#define DPL_P1      1
+#define DPL_P0      0
+#define EN_DPL      2
+#define EN_ACK_PAY  1
+#define EN_DYN_ACK  0
+
+/* Instruction Mnemonics */
+#define R_REGISTER    0x00
+#define W_REGISTER    0x20
+#define REGISTER_MASK 0x1F
+#define ACTIVATE      0x50
+#define R_RX_PL_WID   0x60
+#define R_RX_PAYLOAD  0x61
+#define W_TX_PAYLOAD  0xA0
+#define W_ACK_PAYLOAD 0xA8
+#define FLUSH_TX      0xE1
+#define FLUSH_RX      0xE2
+#define REUSE_TX_PL   0xE3
+#define RF24_NOP      0xFF
+
+/* Non-P omissions */
+#define LNA_HCURR   0
+
+/* P model memory Map */
+#define RPD                  0x09
+#define W_TX_PAYLOAD_NO_ACK  0xB0
+
+/* P model bit Mnemonics */
+#define RF_DR_LOW   5
+#define RF_DR_HIGH  3
+
+#define RF_PWR_MIN 0x00
+#define RF_PWR_LOW 0x01
+#define RF_PWR_MED 0x02
+#define RF_PWR_HIGH 0x03
+
+#define NRF_SUCCESS 0
+#define NRF_SUCCESS 1
+
+
+typedef struct nRF_Handle
+{
+  GPIO_TypeDef*         CSN_Port;
+  uint16_t              CSN_Pin;
+  GPIO_TypeDef*         CE_Port;
+  uint16_t              CE_Pin;
+  SPI_HandleTypeDef*    spihandle;
+  uint8_t* address;
+  uint8_t* rxBuffer;
+  uint8_t* txBuffer;
+}nRF_Handle_t;
+
+typedef enum nRF_RxFifoStatus
+{
+  RXFIFO_PIPE0 = 0,
+  RXFIFO_PIPE1 = 1,
+  RXFIFO_PIPE2 = 2,
+  RXFIFO_PIPE3 = 3,
+  RXFIFO_PIPE4 = 4,
+  RXFIFO_PIPE5 = 5,
+  RXFIFO_NOT_USED = 6,
+  RXFIFO_EMPTY = 7,
+}nRF_RxFifoStatus_t;
+
+
+extern nRF_Handle_t nRFHandle;
+
+void DelayUs(uint32_t );
+void WriteRegister(nRF_Handle_t* , uint8_t , uint8_t );
+void WriteRegisterBytes(nRF_Handle_t* , uint8_t , uint8_t* , uint8_t );
+uint8_t ReadRegister(nRF_Handle_t* , uint8_t );
+void ReadRegisterBytes(nRF_Handle_t* , uint8_t , uint8_t* , uint8_t );
+uint8_t VerifyHandle(nRF_Handle_t* );
+uint8_t nRF_Startup(nRF_Handle_t* );
+void nRF_OpenReadingPipe(nRF_Handle_t* , uint8_t , uint8_t* , uint8_t );
+void nRF_OpenWritingPipe(nRF_Handle_t* , uint8_t* );
+void nRF_CloseReadingPipe(nRF_Handle_t* , uint8_t );
+void nRF_StartListening(nRF_Handle_t* );
+void nRF_StopListening(nRF_Handle_t* );
+void nRF_SetPALevel(nRF_Handle_t* , uint8_t );
+uint8_t nRF_DataAvailable(nRF_Handle_t* );
+void nRF_ReadData(nRF_Handle_t* , uint8_t* , uint8_t );
+void nRF_WriteData(nRF_Handle_t* , uint8_t* , uint8_t );
+
+
+
+#endif
---
+++ Src/nRF24L01.c
@@ -0,0 +1,301 @@
+#include "nRF24L01.h"
+#include "string.h"
+#include "stdlib.h"
+#include "stm32l0xx_hal_rcc.h"
+
+uint8_t pipe0_reading_address[5] = "12345";
+const uint8_t child_pipe[6] = {RX_ADDR_P0, RX_ADDR_P1, RX_ADDR_P2, RX_ADDR_P3, RX_ADDR_P4, RX_ADDR_P5};
+const uint8_t child_pipe_enable[6] = {ERX_P0, ERX_P1, ERX_P2, ERX_P3, ERX_P4, ERX_P5};
+uint16_t txDelay = 0;
+
+// this is ABSOLUTELY only an approximation. not 100% accurate
+void DelayUs(uint32_t n_us)
+{
+  uint32_t clks_per_us = HAL_RCC_GetSysClockFreq()/1000000;
+  // account for # of cycles per instruction and while loop overhead
+  volatile uint32_t nopcount = (clks_per_us *n_us)/12 - 17;
+  while(nopcount-- > 0)
+  {
+    asm("NOP");
+  }
+}
+
+void Delay15Us()
+{
+  uint32_t clks_per_us = HAL_RCC_GetSysClockFreq()/1000000;
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+  asm("NOP");
+}
+
+void WriteCommand(nRF_Handle_t* handle, uint8_t cmd)
+{
+  HAL_SPI_Transmit(handle->spihandle, &cmd, 1, 1000);
+}
+
+void WriteRegister(nRF_Handle_t* handle, uint8_t reg, uint8_t data)
+{
+  uint8_t msg[2] = { (reg | W_REGISTER), data };
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle->spihandle,msg,2U,2000);
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_SET);
+}
+
+void WriteRegisterBytes(nRF_Handle_t* handle, uint8_t reg, uint8_t* data, uint8_t len)
+{
+  uint8_t* txdata = malloc((len+1)*sizeof(uint8_t));
+  txdata[0] = (reg | W_REGISTER);
+  memcpy(&txdata[1], data, len*sizeof(uint8_t));
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle->spihandle, txdata, (len+1)*sizeof(uint8_t), 2000);
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_SET);
+  free(txdata);
+}
+
+uint8_t ReadRegister(nRF_Handle_t* handle, uint8_t reg)
+{
+  uint8_t txdata[2] = { (reg | R_REGISTER), RF24_NOP};
+  uint8_t rxdata[2] = { 0x00, 0x00};
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_RESET);
+  HAL_SPI_TransmitReceive(handle->spihandle, txdata, rxdata, 2, 2000);
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_SET);
+  return rxdata[1];
+}
+
+void ReadRegisterBytes(nRF_Handle_t* handle, uint8_t reg, uint8_t* rxdata, uint8_t len)
+{
+  uint8_t* txdata = malloc((len+1)*sizeof(uint8_t));
+  txdata[0] = (reg | R_REGISTER);
+  memset(&txdata[1], RF24_NOP, len*sizeof(uint8_t));
+  memset(rxdata, 0x00, len*sizeof(uint8_t));
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_RESET);
+  HAL_SPI_TransmitReceive(handle->spihandle, txdata, rxdata, (len+1)*sizeof(uint8_t), 2000);
+  HAL_GPIO_WritePin(handle->CSN_Port, handle->CSN_Pin, GPIO_PIN_SET);
+  free(txdata);
+}
+
+uint8_t GetStatus(nRF_Handle_t* handle)
+{
+  uint8_t rxdata[2] = {0,0};
+  uint8_t txdata[2] = {RF24_NOP,RF24_NOP};
+  HAL_SPI_TransmitReceive(handle->spihandle, txdata, rxdata, 2, 2000);
+  //ReadRegister(handle,NRF_STATUS);
+  return rxdata[0];
+}
+
+// if return is non 0 there is an issue;
+uint8_t VerifyHandle(nRF_Handle_t* pHandle)
+{
+  if(pHandle == NULL || 
+     pHandle->spihandle == NULL ||
+     pHandle->CSN_Port == NULL ||
+     pHandle->CE_Port == NULL) return 1;
+  else return 0;
+}
+
+uint8_t nRF_Startup(nRF_Handle_t* handle)
+{
+  uint8_t regval = 0;
+  
+  // make sure handle has valid pointers
+  if(VerifyHandle(handle)) return 1;
+  
+  HAL_Delay(5);
+  regval = ReadRegister(handle, NRF_CONFIG);
+  WriteRegister(handle, NRF_CONFIG, regval & ~(1 << PWR_UP));
+  HAL_Delay(10);
+  
+  // set up 5 retries with 1500us delay between
+  regval = (5 << 4) | (5);
+  WriteRegister(handle, SETUP_RETR, regval);
+  
+  // set data rate to 1mbps
+  regval = ReadRegister(handle, RF_SETUP);
+  regval &= ~((1 << 5) | (1 << 3));
+  WriteRegister(handle, RF_SETUP, regval);
+  txDelay = 280;
+  if(ReadRegister(handle, RF_SETUP) != regval) return 1;
+  
+  // check special register pertainant to old hardware variant
+  uint8_t before_toggle = ReadRegister(handle, FEATURE);
+  WriteCommand(handle, ACTIVATE);
+  WriteCommand(handle, 0x73);
+  regval = ReadRegister(handle, FEATURE);
+  if(regval)
+  {
+    if(regval == before_toggle)
+    {
+      // module did not experience power-on-reset (#401) idk wtf 0x73 is about but in the other libraries it fixes things
+      WriteCommand(handle, ACTIVATE);
+      WriteCommand(handle, 0x73);
+    }
+    WriteRegister(handle, FEATURE, 0);
+  }
+  
+  // disable dynamic payload
+  WriteRegister(handle, DYNPD, 0);
+  
+  // enable auto-ack on all pipes
+  WriteRegister(handle, EN_AA, 0x3F);
+  
+  // enable receiving on pipes 0 and 1
+  WriteRegister(handle, EN_RXADDR, 3);
+  
+  // set static payload size to 32 bytes for each pipe
+  WriteRegister(handle, RX_PW_P0, 32);
+  WriteRegister(handle, RX_PW_P1, 32);
+  WriteRegister(handle, RX_PW_P2, 32);
+  WriteRegister(handle, RX_PW_P3, 32);
+  WriteRegister(handle, RX_PW_P4, 32);
+  WriteRegister(handle, RX_PW_P5, 32);
+
+  // set address width to 5 bytes
+  WriteRegister(handle, SETUP_AW, 0x03);
+  
+  // set RF channel to 76
+  WriteRegister(handle, RF_CH, 76);
+  
+  // reset status register
+  WriteRegister(handle, NRF_STATUS, (1 << RX_DR)|(1 << TX_DS)|(1 << MAX_RT));
+  
+  // flush tx and rx buffers
+  WriteCommand(handle, FLUSH_RX);
+  WriteCommand(handle, FLUSH_TX);
+  
+  // set config to reflect all isr on isr pin, enable ptx, power up, 16 bit crc
+  regval = (1 << EN_CRC) | (1 << CRCO) | (1 << MASK_TX_DS) | (1 << MASK_MAX_RT);
+  WriteRegister(handle, NRF_CONFIG, regval);
+  
+  // set power up bit 
+  regval |= 1 << PWR_UP;
+  WriteRegister(handle, NRF_CONFIG, regval);
+  
+  // delay 5 ms after power up
+  HAL_Delay(5);
+  
+  // verify config register is properly set
+  regval = ReadRegister(handle, NRF_CONFIG);
+  if(regval == (1 << EN_CRC) | (1 << CRCO) | (1 << PWR_UP)) return 0;
+  else return 1;
+}
+
+void nRF_OpenReadingPipe(nRF_Handle_t* handle, uint8_t pipe, uint8_t* address, uint8_t addr_width)
+{
+  // store address if pipe 0 to recall later for openwritingpipe
+  if(pipe == 0)
+  {
+    memcpy(pipe0_reading_address, address, addr_width);
+  }
+  // only transmit one byte of address if pipe >= 2
+  if(pipe < 2)
+  {
+    WriteRegisterBytes(handle, child_pipe[pipe], address, addr_width);
+  }
+  else
+  {
+    WriteRegisterBytes(handle, child_pipe[pipe], address, 1);
+  }
+  // enable rx pipe
+  uint8_t regval = ReadRegister(handle, EN_RXADDR);
+  WriteRegister(handle, EN_RXADDR, regval|(1 << child_pipe_enable[pipe]));
+}
+
+void nRF_CloseReadingPipe(nRF_Handle_t* handle, uint8_t pipe)
+{
+  uint8_t regval = ReadRegister(handle, EN_RXADDR);
+  regval &= ~(1 << child_pipe_enable[pipe]);
+  WriteRegister(handle, EN_RXADDR, regval);
+}
+
+void nRF_OpenWritingPipe(nRF_Handle_t* handle, uint8_t* address)
+{
+  WriteRegisterBytes(handle, RX_ADDR_P0, address, 5);
+  WriteRegisterBytes(handle, TX_ADDR, address, 5);
+}
+
+void nRF_StartListening(nRF_Handle_t* handle)
+{
+  // clear interrupt bits
+  WriteRegister(handle, NRF_STATUS, (1<<RX_DR)|(1<<TX_DS)|(1<<MAX_RT));
+  // set receive mode bit 
+  uint8_t regval = ReadRegister(handle, NRF_CONFIG);
+  WriteRegister(handle, NRF_CONFIG, regval | (1 << PRIM_RX));
+  
+  // restore pipe 0 address
+  if(pipe0_reading_address[0] > 0)
+  {
+    WriteRegisterBytes(handle, RX_ADDR_P0, pipe0_reading_address, 5);
+  }
+  else
+  {
+    nRF_CloseReadingPipe(handle, 0);
+  }
+  
+  // set chip enable 
+  HAL_GPIO_WritePin(handle->CE_Port, handle->CE_Pin, GPIO_PIN_SET);
+  DelayUs(130);
+  
+  
+}
+
+void nRF_StopListening(nRF_Handle_t* handle)
+{
+  HAL_GPIO_WritePin(handle->CE_Port, handle->CE_Pin, GPIO_PIN_RESET );
+  HAL_Delay(1);
+  WriteCommand(handle, FLUSH_TX);
+  uint8_t regval = ReadRegister(handle, NRF_CONFIG);
+  regval &= ~(1 << PRIM_RX);
+  WriteRegister(handle, NRF_CONFIG, regval);
+  regval = ReadRegister(handle, EN_RXADDR);
+  regval |= (1 << child_pipe_enable[0]);
+  WriteRegister(handle, EN_RXADDR, regval);
+}
+     
+void nRF_SetPALevel(nRF_Handle_t* handle, uint8_t level)
+{
+  uint8_t regval = ReadRegister(handle, RF_SETUP) & 0xF8;
+  regval |= ((level << 1) + 1);
+  WriteRegister(handle, RF_SETUP, regval);
+}
+
+// returns 1 if data available and 0 otherwise
+uint8_t nRF_DataAvailable(nRF_Handle_t* handle)
+{
+  uint8_t fifoReady = ReadRegister(handle, FIFO_STATUS) & (1 << RX_FULL);
+  uint8_t status = (GetStatus(handle) >> RX_P_NO) && 0x07;
+  if( status <= 5 && fifoReady) return 1;
+  else return 0;
+}
+
+void nRF_WriteData(nRF_Handle_t* handle, uint8_t* data, uint8_t len)
+{
+  WriteRegisterBytes(handle, W_TX_PAYLOAD, data, len);
+  HAL_GPIO_WritePin(handle->CE_Port, handle->CE_Pin, GPIO_PIN_SET);
+  Delay15Us();
+  // wait until tx data sent flag is set or max number of retreies reached
+  while(!(ReadRegister(handle, NRF_STATUS) & ((1 << TX_DS)|(1 << MAX_RT)))) 
+  {
+  }
+  HAL_GPIO_WritePin(handle->CE_Port, handle->CE_Pin, GPIO_PIN_RESET);
+  Delay15Us();
+  WriteRegister(handle,NRF_STATUS, ReadRegister(handle, NRF_STATUS) | (1 << TX_DS) | (1 << MAX_RT));
+}
+     
+void nRF_ReadData(nRF_Handle_t* handle, uint8_t* rxbuff, uint8_t len)
+{
+  ReadRegisterBytes(handle, R_RX_PAYLOAD, rxbuff, len);
+  // clear data rx interrupt flag
+  WriteRegister(handle, NRF_STATUS, ReadRegister(handle,NRF_STATUS) | (1 << RX_DR));
+}
